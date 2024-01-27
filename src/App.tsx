a5e516a1385812ce798353c5d6f001cb27de5f83
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-01-27 17:18:58 +0900

    feat : routes components

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,7 @@
-import { ActionFunction, createBrowserRouter, LoaderFunction, RouterProvider } from 'react-router-dom'
-import Layout from './components/Layout.tsx'
+import Routes from './routes'
 
-interface RouteCommon {
-  loader?: LoaderFunction
-  action?: ActionFunction
-  ErrorBoundary?: React.ComponentType<any>
-}
-
-interface IRoute extends RouteCommon {
-  path: string
-  label: string
-  active?: boolean
-  Element: React.ComponentType<any>
-}
-
-interface Pages {
-  [key: string]: {
-    default: React.ComponentType<any>
-  } & RouteCommon
-}
-
-const pages: Pages = import.meta.glob('./pages/**/*.tsx', { eager: true })
-
-const routes: IRoute[] = []
-for (const path of Object.keys(pages)) {
-  const fileName = path.match(/\.\/pages\/(.*)\.tsx$/)?.[1]
-  if (!fileName) {
-    continue
-  }
-
-  const normalizedPathName = fileName.includes('$') ? fileName.replace('$', ':') : fileName.replace(/\/index/, '')
-
-  routes.push({
-    path: fileName === 'index' ? '/' : `/${normalizedPathName.toLowerCase()}`,
-    label: normalizedPathName,
-    Element: pages[path].default,
-    loader: pages[path]?.loader as LoaderFunction | undefined,
-    action: pages[path]?.action as ActionFunction | undefined,
-    ErrorBoundary: pages[path]?.ErrorBoundary,
-  })
-}
-
-const router = createBrowserRouter(
-  routes.map(({ Element, ErrorBoundary, ...rest }) => ({
-    ...rest,
-    element: (
-      <Layout routes={routes}>
-        <Element />
-      </Layout>
-    ),
-    ...(ErrorBoundary && { errorElement: <ErrorBoundary /> }),
-  })),
-)
-
-const App = () => {
-  return <RouterProvider router={router} />
+function App() {
+  return <Routes />
 }
 
 export default App
