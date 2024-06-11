fa3463f8e23002c592a5443d48ba0b37b4388aac
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-06-11 23:02:08 +0900

    feat: authenticator, signIn 합치기, React.StrictMode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -7,9 +6,7 @@ import './index.css'
 const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <App />
+  </GoogleOAuthProvider>,
 )
--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -3,25 +3,46 @@ import { MemberRegisterTypes } from '@/types/type.ts'
 import { useEffect } from 'react'
 import GoogleBtn from '@/pages/auth/googleBtn.tsx'
 import NaverBtn from '@/pages/auth/naverBtn.tsx'
+import { SocialLoginRequest } from '@/types/socialLoginRequest.ts'
+import AuthService from '@/apis/AuthService.ts'
+import { isSocialUserResponse } from '@/types/socialUserResponse.ts'
+import { isSocialLoginResponse } from '@/types/socialLoginResponse.ts'
+import { setAuthorizationToken } from '@/libs/axios.ts'
 
 const SignIn = () => {
   const navigate = useNavigate() // useNavigate 훅 사용
-  const handleLoginSuccess = (tokenResponse: any, provider: MemberRegisterTypes) => {
-    console.log(tokenResponse)
-    navigate('/auth/authenticator', {
-      state: {
-        accessToken: tokenResponse.access_token,
-        memberRegisterType: provider, // provider 값을 설정
-      },
-    })
-  }
+
   const handleCallback = () => {
     const hashParams = new URLSearchParams(window.location.hash.substring(1))
     const token = hashParams.get('access_token')
     const storedProvider = localStorage.getItem('provider') as MemberRegisterTypes | null
     if (token && storedProvider) {
-      handleLoginSuccess({ access_token: token }, storedProvider)
+      doSignIn(token, storedProvider)
     }
+  }
+  function doSignIn(token: string, provider: MemberRegisterTypes) {
+    const socialLoginRequest: SocialLoginRequest = {
+      token: token,
+      memberRegisterType: provider,
+      oauthType: 'TOKEN',
+    }
+    AuthService.signInWithSocial<any>(socialLoginRequest)
+      .then((response) => {
+        if (response.status === 200) {
+          if (isSocialUserResponse(response.data)) {
+            navigate('/auth/signUp', { state: response.data })
+          }
+          if (isSocialLoginResponse(response.data)) {
+            localStorage.setItem('access_token', response.data.access_token)
+            localStorage.setItem('refresh_token', response.data.refresh_token)
+            setAuthorizationToken(response.data.access_token)
+            navigate('/')
+          }
+        }
+      })
+      .catch((error: { [key: string]: string | number }) => {
+        alert(error.message ?? error)
+      })
   }
   useEffect(() => {
     handleCallback()
